0ef40e3ec9c81d25cece2816d1281e2c47c31156
Janvarev Vladislav <janvarev@gmail.com>
2023-04-29 19:16:26 +0300

    initial commit

---
+++ start-server.bat
@@ -0,0 +1,19 @@
+@echo off
+
+@echo Starting the kobold_api_multilang_proxy..
+
+cd /D "%~dp0"
+
+set MAMBA_ROOT_PREFIX=%cd%\installer_files\mamba
+set INSTALL_ENV_DIR=%cd%\installer_files\env
+
+if not exist "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" (
+  call "%MAMBA_ROOT_PREFIX%\micromamba.exe" shell hook >nul 2>&1
+)
+call "%MAMBA_ROOT_PREFIX%\condabin\micromamba.bat" activate "%INSTALL_ENV_DIR%" || ( echo MicroMamba hook not found. && goto end )
+cd kobold_api_multilang_proxy
+
+call python server.py
+
+:end
+pause
